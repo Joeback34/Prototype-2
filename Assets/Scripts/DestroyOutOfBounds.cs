97d03d5db1b44678ffc38b4379b23d6792d41fbb
Joeback34 <joebackthehero@gmail.com>
2023-02-14 13:45:55 -0600

    Initial commit

---
+++ Assets/Scripts/DestroyOutOfBounds.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOutOfBounds : MonoBehaviour
+{
+    
+    private void Start()
+    {
+
+
+    }
+    private float topBound = 30;
+    private float lowerBound = -10;
+
+    void Update()
+    {   // if an object goes past the players view, removes the object.
+        if (transform.position.z > topBound)
+        {
+            Destroy(gameObject); 
+        }
+        else if (transform.position.z < lowerBound)
+        {
+            Debug.Log("Game Over!");
+            Destroy(gameObject);
+        }
+    }
+}
